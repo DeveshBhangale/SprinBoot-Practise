77a61c9769a3b404506f232e8123ca1ae320f296
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-23 23:16:22 +0530

    [Devesh]: added SLF4j Logger module

--- demobean/src/main/java/com/bridgelabz/springbootpractise/DemobeanApplication.java
+++ demobean/src/main/java/com/bridgelabz/springbootpractise/DemobeanApplication.java
@@ -2,21 +2,23 @@ package com.bridgelabz.springbootpractise;
 
 import com.bridgelabz.springbootpractise.component.DemoBean;
 import com.bridgelabz.springbootpractise.controller.HelloRestController;
-import com.bridgelabz.springbootpractise.controller.HelloWebController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemobeanApplication {
+	public static final Logger logger=LoggerFactory.getLogger(DemobeanApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		logger.info("Hello, Welcome to Demo");
 		ConfigurableApplicationContext context=SpringApplication.run(DemobeanApplication.class, args);
 		DemoBean demoBean=context.getBean(DemoBean.class);
-		System.out.println("Demo Bean= "+demoBean.toString());
-		System.out.println(context.getBean(HelloRestController.class));
+		logger.debug("Demo Bean= "+demoBean.toString());
+		logger.debug(""+context.getBean(HelloRestController.class));
 		
 	}
 
